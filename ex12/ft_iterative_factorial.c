557bb9621e682be915d5730da10747fa7192ebf5
Baker Satou <bsatou@ox-s5.21-school.ru>
2019-09-02 20:00:25 +0300

    update ex12

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: bsatou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 11:11:11 by bsatou            #+#    #+#             */
-/*   Updated: 2019/07/06 11:19:36 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/02 19:56:32 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		ft_iterative_factorial(int nb)
 	result = 1;
 	fack_gg = 1;
 	if ((nb < 0) || (nb > 12))
-                return (0);
+		return (0);
 	if (nb == 1)
 		return (1);
 	while (i < nb - 1)
